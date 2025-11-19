57fa8b3e43293d228df2ce167be4117c246817b0
Jacob Weedman <n.jacob.weedman@gmail.com>
2025-11-18 18:30:02 -0700

    Sector 1 Boss WIP
    Boss movement, switching attack modes, arena improvements

--- Assets/Scripts/BossScripts/Sector1BossScript.cs
+++ Assets/Scripts/BossScripts/Sector1BossScript.cs
@@ -12,6 +12,8 @@ public class Sector1BossScript : MonoBehaviour
 // Designed by Jacob Weedman
 
     GameObject Player;
+    GameObject Barrel;
+    GameObject Target;
 
     public int Phase = 1;
     public float Health = 1000;
@@ -20,12 +22,31 @@ public class Sector1BossScript : MonoBehaviour
     public int DeployTimer = 4000; // ms
     public List<GameObject> DeployLocations;
 
+    public int Choice; // 0 == deploy, 1 == shoot
+    float InititialChoiceCountdown = 5; // sec
+    float ChoiceCountdown = 5; // sec
+    float InitialBulletInterval = 0.1f;
+    float BulletInterval = 0.1f;
+    float InitialMoveCountdown = 2; //sec
+    float MoveCountdown = 6; // sec
+
+    int WeaponDamage = 7;
+    int WeaponRange = 50;
+    int WeaponProjectileSpeed = 40;
+    int WeaponRandomSpread = 5;
+
+    float MovementSpeed = 2f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Player = GameObject.FindWithTag("Player");
+        Barrel = transform.Find("Barrel").gameObject;
+        Target = GameObject.Find("BossTarget");
+        Target.transform.position = transform.position;
+
         foreach (Transform spawner in transform)
-            {
+        {
             if (spawner.name == "SwitchbladeSpawner")
             {
                 DeployLocations.Add(spawner.gameObject);
@@ -38,6 +59,11 @@ public class Sector1BossScript : MonoBehaviour
     void FixedUpdate()
     {
 
+        if (Phase == 1)
+        {
+            BackgroundMove();
+        }
+
         if (Health <= 500)
         {
             Phase = 2;
@@ -45,7 +71,7 @@ public class Sector1BossScript : MonoBehaviour
 
         switch (Phase)
         {
-            case 1:
+            case 1: // Phase 1
 
                 if (Health >= 900)
             {
@@ -64,13 +90,51 @@ public class Sector1BossScript : MonoBehaviour
                 DeployTimer = 50;
             }
 
+            ChoiceCountdown -= Time.deltaTime;
+            if (ChoiceCountdown <= 0)
+            {
+                ChoiceCountdown = InititialChoiceCountdown;
+                Choice = UnityEngine.Random.Range(0, 2);
+            }
+
+            MoveCountdown -= Time.deltaTime;
+            if (Vector2.Distance(Target.transform.position, transform.position) <= 0.1)
+            {
+                if (MoveCountdown <= 0 )
+                {
+                MoveCountdown = InitialMoveCountdown;
+                Target.transform.position = new Vector2(UnityEngine.Random.Range(-20, 13), UnityEngine.Random.Range(2, 9));
+                }
+            }
+            else
+            {
+                MoveCountdown = InitialMoveCountdown   ;
+            }
+
+            switch (Choice)
+            {
+                case 0: // deploy
                 if (canDeploy)
                 {
                     Deploy();
                 }
+                break;
+
+                case 1:
+                BulletInterval -= Time.deltaTime;
+                if (BulletInterval <= 0)
+                    {
+                        BulletInterval = InitialBulletInterval;
+                        Shoot();            
+                    }
 
                 break;
-            case 2:
+
+                default:
+                break;               
+            }
+                break;
+            case 2: // Phase 2
 
                 break;
             default:
@@ -98,4 +162,27 @@ public class Sector1BossScript : MonoBehaviour
 
         canDeploy = true;
     }
+
+    void Shoot()
+    {
+        // Create Projectile
+        GameObject BulletInstance;
+        BulletInstance = Instantiate(GameObject.Find("EnemyProjectile"), Barrel.transform.position, Quaternion.LookRotation(transform.position - GameObject.FindGameObjectWithTag("Player").transform.position + new Vector3(UnityEngine.Random.Range((-1 * WeaponRandomSpread), WeaponRandomSpread), UnityEngine.Random.Range((-1 * WeaponRandomSpread), WeaponRandomSpread), 0)));
+        //BulletInstance.transform.parent = transform;
+
+        // Variables
+        BulletInstance.GetComponent<EnemyProjectileScript>().WeaponDamage = WeaponDamage;
+        BulletInstance.GetComponent<EnemyProjectileScript>().WeaponRange = WeaponRange;
+
+        // Send it on it's way
+        BulletInstance.GetComponent<Rigidbody2D>().linearVelocity = BulletInstance.transform.forward * -1 * WeaponProjectileSpeed;
+        BulletInstance.transform.rotation = Quaternion.Euler(new Vector3(0, 0, Vector2.SignedAngle(Vector2.right, BulletInstance.transform.forward) - 90));
+                
+    }
+
+    void BackgroundMove()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(Target.transform.position.x, Target.transform.position.y, transform.position.z), MovementSpeed * Time.deltaTime);
+    }
+
 }
